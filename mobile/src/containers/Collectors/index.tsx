8650a863c98d795b161f0249097369d33d13a1f4
Lucas Ferreira <lucas.htferreira@outlook.com>
2020-06-04 14:03:06 -0300

    Started working on Collectors page on mobile

--- mobile/src/containers/Collectors/index.tsx
+++ mobile/src/containers/Collectors/index.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import { Feather as Icon } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { SvgUri } from 'react-native-svg';
+
+import * as Styled from './styles';
 
 const Collectors = () => {
+	const navigation = useNavigation();
+
+	function navBackHandler() {
+		navigation.goBack();
+	}
+
 	return (
-		<View>
-			<Text>Hello World</Text>
-		</View>
+		<>
+			<Styled.Collectors>
+				<TouchableOpacity>
+					<Icon
+						name='arrow-left'
+						size={20}
+						color='#34cb79'
+						onPress={navBackHandler}
+					/>
+				</TouchableOpacity>
+
+				<Styled.Title>Bem vindo.</Styled.Title>
+				<Styled.Description>
+					Encontre no mapa um ponto de coleta.
+				</Styled.Description>
+
+				<Styled.MapContainer>
+					<Styled.Map />
+				</Styled.MapContainer>
+			</Styled.Collectors>
+			<Styled.ItemsContainer>
+				<Styled.Item onPress={() => {}}>
+					<SvgUri width={42} height={42} uri={''} />
+				</Styled.Item>
+			</Styled.ItemsContainer>
+		</>
 	);
 };
 
